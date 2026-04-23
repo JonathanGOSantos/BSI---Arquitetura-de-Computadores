c4ccac514c5f7d2dab20998a3b519668c78c9a89
Enzo <enzotheodg@gmail.com>
2026-04-23 19:10:57 -0300

    Criação dos metodos de processamento de linha

--- src/main/java/conversormips/ConversorMips.java
+++ src/main/java/conversormips/ConversorMips.java
@@ -1,5 +1,7 @@
 package conversormips;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import conversormips.dominio.entidades.InstrucaoMIPS;
@@ -7,15 +9,48 @@ import conversormips.dominio.enums.EnumInstrucoes;
 
 public class ConversorMips {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConversorMips conversor = new ConversorMips();
+        String caminho = "C:\\Teste_Arquitetura\\";
+
+        for (int i = 1; i <= 2; i++) {
+            String nomeArquivo = "TESTE-0"+i;
+            conversor.gravarArquivo(caminho+nomeArquivo+"-RESULTADO.txt", conversor.processarLinhas(conversor.lerArquivo(caminho+nomeArquivo+".txt")));
+        }
+    }
+    public List<String> processarLinhas(List<String> linhas){
+        List<String> linhasBinarias = new ArrayList<>();
+        for (String linha: linhas){
+            linhasBinarias.add(processarLinha(linha).getBinario());
+        }
+        return linhasBinarias;
     }
 
     public List<String> lerArquivo(String caminho) {
-        throw new UnsupportedOperationException("Método ainda não implementado.");
+        List<String> linhas = new ArrayList<>();
+        try(BufferedReader  br = new BufferedReader(new FileReader(caminho))){
+            String linha = br.readLine();
+            while(linha != null){
+                linhas.add(linha);
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return linhas;
+
     }
 
-    public void gravarArquivo(String caminho, List<String> conteudo) {
-        throw new UnsupportedOperationException("Método ainda não implementado.");
+    public void gravarArquivo(String caminho, List<String> linhas) {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(caminho))){
+            for (String linha: linhas){
+                bw.write(linha);
+                bw.newLine();
+            }
+
+        }catch (IOException e){
+            throw new RuntimeException();
+        }
+
     }
 
     public InstrucaoMIPS processarLinha(String linha) {
